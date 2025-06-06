98c263eb76a0e623a46f512594e237d90b14f161
tan-tanni <tanyyy101@gmail.com>
2025-06-06 19:46:24 +0000

    upate s tests

--- index.py
+++ index.py
@@ -1,18 +1,30 @@
 
 import math
+from flask import Flask, render_template, request, redirect, url_for
 
 import matplotlib.pyplot as plt
 # import install matplotlib.pyplot as plt 
 
-professions = { "Psychologist": "Working with people's emotions and behavior, helping to solve personal problems",
-"Programmer": "Creating and developing software, working with code",
-"Designer":  "Interior design, clothing, urban environment and even  applications app",
-"Journalist": "Searching for and covering events, creating articles and reports",
-"Teacher": "Teaching children or adults, developing educational materials",
-"Actor": "Creating an artistic image, participating in plays and films",
-"Business Trainer": "Training and developing skills of company employees", "Ecologist": "Studying and preserving the natural environment", 
-"Agronomist": "Growing plants, caring for soil and climate", "Doctor": "Treating patients, diagnosing diseases","SalesManager": "Developing a client base, concluding deals", "Lyer": "Legal protection of interests, drafting contracts",  "Artist": "Creating paintings, graphics, illustrations", "Engineer": "Designing and creating technical devices and systems", "Marketer": "Promotion of goods and services, market analysis"
+app = Flask(__name__)
 
+with open ('questions.json', 'r', encoding = 'uft-8') as f:
+
+professions = { "Psychologist": "Working with people's emotions and behavior, helping to solve personal problems",
+"Programmer": "Creating and developing software, working with code.  Information technology (IT) software development, systems analysis, cybersecurity, etc. ",
+"Designer":  "Fashion, animation, video and audio production, interior design, clothing, urban environment and even  applications app",
+"Journalist": "Searching for and covering events, creating articles and reports Journalism and media content creation, news reporting, blogging, PR..",
+"Teacher": "Teaching children or adults, developing educational materials. Education and pedagogy teaching, upbringing, organizing the educational process.",
+"Actor": "Creating an artistic image, participating in plays and films. graphic design, fashion, animation, video and audio production.",
+"Business Trainer": "Training and developing skills of company employees", 
+"Ecologist": "Studying and preserving the natural environment. Finance and banking managing capital, investments, accounting, corporate finances.", 
+"Agronomist": "Growing plants, caring for soil and . Agriculture and farming growing plants, raising animals, processing products.", 
+"Doctor": "Treating patients, diagnosing diseases. Medicine and healthcare treatment, diagnostics, pharmaceuticals, psychology.",
+"SalesManager": "Developing a client base, concluding deals. Marketing and advertising promoting goods and services, working with a brand, digital marketing.", 
+"Lyer": "Legal protection of interests, drafting contracts. Jurisprudence legal protection, consulting, litigation.", 
+"Artist": "You are a creative person, so create) Creating paintings, graphics, writer. Design and creative industries graphic design, fashion, animation, video and audio production.", 
+"Engineer": "Designing and creating technical devices. Manufacturing and engineering industrial production, automation, mechanical engineering.", 
+"Marketer": "Promotion of goods and services, market analysis",
+"Tourism" : "Tourism and hospitality  travel organization, work in hotels, excursion services."
 }
 
 scores = {p: 0 for p in professions}
@@ -43,6 +55,83 @@ questions = [
         'how to make money':'Business Trainer' and 'SalesManager'
         'how to help people and the worl':'Lyer' and 'Psychologist'
     }
+
+    # Clarifying que. for "extrovert + humanities"
+    {
+        'text': 'What role do you want to play in the project?',
+        'options': ['Host', 'Writer', 'Organizer'],
+        'type': ['extro', 'human'],
+        'mapping': {
+        'Host': 'Actor',
+        'Writer': 'Journalist',
+        'Organizer': 'Business coach'}
+    },
+    {
+        'text': 'What do you find more interesting to read?',
+        'options': ['Novels', 'Biographies', 'Fashion magazines'],
+        'type': ['extro', 'human'],
+        'mapping': {'Novels': 'Writer',
+        'Biographies': 'Journalist',
+        'Fashion Magazines': 'Designer'
+        }
+    }, 
+    {
+        'text': 'What do you like to read?',
+        'options': ['Novels', 'Biographies', 'Fashion magazines'],
+        'type': ['extro', 'human'],
+        'mapping': {
+        'Novels': 'Writer',
+        'Biographies': 'Journalist',
+        'Fashion magazines': 'Interface designer'
+    }
+    },
+
+    # que "extrovert + techie"
+    {
+        'text': 'Which tool do you prefer?',
+        'options': ['Book', 'Hammer', 'Laptop'],
+        'type': ['extro', 'tech'],
+        'mapping': {
+        'Book': 'Manager sales',
+        'Hammer': 'Engineer',
+        'Laptop': 'Programmer'
+        }
+    },
+    {
+        'text': 'Which tool do you prefer?',
+        'options': ['Hammer', 'Computer', 'Microscope'],
+        'type': ['intro', 'tech'],
+        'mapping': {
+        'Hammer': 'Engineer',
+        'Computer': 'Programmer',
+        'Microscope': 'Ecologist' , 'Agronomist'}
+    },
+        # Clarifying questions for "introvert + techie"
+    {
+        'text': 'What would you do with pleasure?',
+        'options': ['Create a diagram', 'Command and other', 'Study the documentation'],
+        'type': ['intro', 'tech'],
+        'mapping': {
+        'Create a diagram': 'Engineer',
+        'Command and other': 'Business Trainer',
+        'Study the documentation': 'Programmer'
+        }
+    },
+    {
+        'text' : 'What gives you strength? Choose one option that is closer to you:'
+        'options': ['When I solve a difficult problem',
+        'When I help someone understand themselves', 'When I create something new',
+        'When I feel stability and control',
+        'When I am appreciated and accepted'],
+        'mapping': [
+        'When I solve a difficult problem': 'Engineer'and 'Ecologist',
+        'When I help someone understand themselves': 'Psychologist' and 'Teacher',
+        'When I create something new': 'Artist' and 'Designer',
+        'When I feel stability and control': 'Lyer',
+        'When I am appreciated and accepted': 'Tourism' and 'Agronomist'
+        ]
+ }
+    
 ]
 
 
@@ -120,10 +209,9 @@ def run_test():
 run_test()
 
 
+if __name__ == '__main__':
+    app.run(debug = True)
 
 '''
-
-def  main:
-    qwestions()
-    show_results()
+d
     '''
